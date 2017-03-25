94789f265a8107c1a7e47ac27f3950618e0fa5dc
wasper256 <andremorhun@gmail.com>
2017-03-25 07:24:46 +0200

    fixed input worker form

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request, flash, g
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__)
 app.secret_key = 'some_secret'
@@ -49,8 +51,16 @@ def newworker():
     if request.method == 'POST':
         name = request.form['name']
         surname = request.form['surname']
-        print(name)
-        print(surname)
+        email = request.form['email']
+        phone = request.form['phone']
+        bdate = request.form['bdate']
+        print(bdate)
+        bdate = datetime.strptime(bdate, "%Y-%m-%d")
+
+        print(surname, name, email, phone, bdate)
+        db.session.add(Worker(name, surname, email, phone, bdate, None, None, False))
+        db.session.commit()
+        flash("New worker was added")
     return render_template('newworker.html', error=error)
 
 
